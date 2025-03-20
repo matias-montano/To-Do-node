c2fe68ebb36fc1eeb005cd5733450c5554903885
Matias Montano <matias.montano.cobas@gmail.com>
2025-03-20 16:58:00 -0300

    chore: remove unused Babel and Jest configuration files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,17 +1,30 @@
 module.exports = {
+  parser: '@babel/eslint-parser',
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true, // Habilita JSX
+    },
+    requireConfigFile: false, 
   },
   env: {
     es6: true,
     node: true,
+    browser: true, 
     jest: true,
   },
   extends: [
     'eslint:recommended',
+    'plugin:react/recommended', 
   ],
+  plugins: ['react'],
+  settings: {
+    react: {
+      version: 'detect', 
+    },
+  },
   rules: {
-    // Añade tus reglas personalizadas aquí
+    // reglas personalizadas
   },
 };
